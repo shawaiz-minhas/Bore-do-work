7446ae0cbe8745f9a485809921649cb42b7267de
shawaiz-minhas <shawaizminhas456@gmail.com>
2022-10-03 19:17:36 +0500

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+document.getElementById("hemlo").addEventListener("click",function(){
+    fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => 
+       {
+        document.getElementById("work").textContent = data.activity
+       })
+})
+
+// document.getElementById("hemlo").addEventListener("click", function() {
+//     fetch("https://apis.scrimba.com/bored/api/activity")
+//       .then(response => response.json())
+//       .then(data => {
+//         document.getElementById("work").textContent = data.activity
+//       })
+//   })
